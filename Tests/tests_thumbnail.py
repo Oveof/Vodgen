34c7cad5f286e2ddaed1f1aa1b25b95eae4fea11
oveof <ovedy@oftedal.com>
2021-12-01 00:34:41 +0100

    test123

--- Tests/tests_thumbnail.py
+++ Tests/tests_thumbnail.py
@@ -1,9 +1,8 @@
 """Thumbnail tests"""
 import unittest
-import os
 import sys
+sys.path.append("..")
 from Vodgen import thumbnail
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class MatchInfoTests(unittest.TestCase):
     """Tests match info methods"""
@@ -12,5 +11,5 @@ class MatchInfoTests(unittest.TestCase):
         """Tests the parser who shortens title"""
         match = thumbnail.MatchInfo("Brult")
 
-        match.set_tournament_round("R1")
-        self.assertEqual(match.tournament_round, "Round 1")
+        match.set_tournament_round("Round 1")
+        self.assertEqual(match.tournament_round, "R1")
