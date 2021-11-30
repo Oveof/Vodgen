311f35f94cddfd7e5e6f187a292f2e5a70d88313
oveof <ovedy@oftedal.com>
2021-11-30 20:40:05 +0100

    test123

--- vodgen/videocutter.py
+++ vodgen/videocutter.py
@@ -8,6 +8,7 @@ Creates a video based on given parameters, has experiemntal mode if given the co
 """
 def create_video(original_file, start_time, end_time, result_file, codec=None):
     if codec is not None:
+        # pylint: disable=line-too-long
         call = f"echo y|ffmpeg -i {original_file} -vcodec  -ss {start_time} -to {end_time} -c:v copy -c:a copy {result_file}"
         os.system(call)
 
