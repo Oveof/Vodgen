dad8a825f6733d2887444277a54d6ced7ac98342
oveof <ovedy@oftedal.com>
2021-12-01 15:48:00 +0100

    f

--- Tests/test_thumbnail.py
+++ Tests/test_thumbnail.py
@@ -1,9 +1,5 @@
 """Thumbnail tests"""
 import unittest
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from Vodgen import thumbnail
 
 class MatchInfoTests(unittest.TestCase):
