cfc9db04a78f77da4118477a642082aa80e351ad
oveof <ovedy@oftedal.com>
2021-12-01 17:11:33 +0100

    lol

--- Tests/test_thumbnail.py
+++ Tests/test_thumbnail.py
@@ -1,12 +1,12 @@
 """Thumbnail tests"""
 import unittest
-from Vodgen import thumbnail
+from thumbnail import MatchInfo
 
 class MatchInfoTests(unittest.TestCase):
     """Tests match info methods"""
     def tests_set_tournament_round(self):
         """Tests the parser who shortens title"""
-        match = thumbnail.MatchInfo("Brult")
+        match = MatchInfo("Brult")
 
         match.set_tournament_round("Round 1")
         self.assertEqual(match.tournament_round, "R1")
