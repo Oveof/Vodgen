38b6fe30094e01e2c67500ed17d63e89be79ee63
oveof <ovedy@oftedal.com>
2021-12-09 22:47:10 +0100

    backburner, forgot commits

--- vodgen/main.py
+++ vodgen/main.py
@@ -1,9 +1,58 @@
 """Vodgen app"""
 import sys
 import json
+import re
 from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox,
-    QFileDialog, QLabel, QLineEdit, QMainWindow, QPushButton, QVBoxLayout, QWidget)
-from thumbnail import Thumbnail, Player, Config, ImageInfo
+    QFileDialog, QLabel, QLineEdit, QMainWindow, QPlainTextEdit, QPushButton, QVBoxLayout, QWidget)
+from thumbnail import Thumbnail, Player, Config, ImageInfo, MatchInfo
+
+def formatTitle(title):
+    game_info = title.split(": ")[1].split(" - ")[0]
+    tournament_round = ' '.join(game_info.split(' ')[-2:])
+    #gameRound = gameInfo.split(' ', 2)
+    game_name = game_info.split(' ')[0]
+    if "Winners" in game_info:
+        game_name = game_info.split(' Winners')[0]
+    elif "Losers" in game_info:
+        game_name = game_info.split(' Losers')[0]
+    elif "Grand Finals" in game_info:
+        game_name = game_info.split(' Grand')[0]
+    else:
+        raise Exception("Error")
+
+    player_info = title.split("-")[1]
+    team1 = player_info.split("vs")[0].strip()
+    team1_players = team1.split("(")[0].split(" + ")
+    team1_characters_search = re.search(r"\(([A-Za-z0-9_, .é+]+)\)", team1)
+    if team1_characters_search == None:
+        raise Exception(NameError)
+    team1_characters = team1_characters_search.group(1).split(", ")[0].split(" + ")
+
+    team2 = player_info.split("vs")[1].strip()
+    team2_players = team2.split("(")[0].split(" + ")
+    team2_characters_search = re.search(r"\(([A-Za-z0-9_, .é+]+)\)", team2)
+    if team2_characters_search == None:
+        raise Exception(NameError)
+    team2_characters = team2_characters_search.group(1).split(", ")[0].split(" + ")
+    
+    player_names = team1_players + team2_players
+    player_characters = team1_characters + team2_characters
+
+    player_list = []
+    for x in range(len(player_names)):
+        if len(player_names) / 2 > x:
+            team_num = 0
+        else:
+            team_num = 1
+        player_list.append(Player(player_names[x], player_characters[x], team_num, x+1))
+
+    
+    return player_list, tournament_round, game_name
+
+
+
+
+
 
 class MainWindow(QMainWindow):
     """Main UI window"""
@@ -13,27 +62,34 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Lmao")
         layout = QVBoxLayout()
 
-        choose_file = QPushButton("Choose stream file")
-        choose_file.clicked.connect(self.choose_file)
-        choose_region = QComboBox()
-        choose_game = QComboBox()
+        self.choose_stream = QPushButton("Choose stream file")
+        self.choose_stream.clicked.connect(self.choose_file)
+        self.choose_region = QComboBox()
+        self.choose_game = QComboBox()
+        self.choose_banner = QPushButton("Choose Banner")
+        self.choose_banner.clicked.connect(self.choose_file)
 
         #Adds regions form config to dropdown menu
         with open('config.json', encoding="utf-8") as file:
             config = json.load(file)
             for attribute, _ in config["tournament"].items():
-                choose_region.addItem(attribute)
+                self.choose_region.addItem(attribute)
             for attribute, _ in config["game"].items():
-                choose_game.addItem(attribute)
+                self.choose_game.addItem(attribute)
 
-        only_thumbnails = QCheckBox("Only thumbnails")
-        create_videos_button = QPushButton("Generate Video")
+        self.only_thumbnails = QCheckBox("Only thumbnails")
+        self.create_videos_button = QPushButton("Generate VoDs and thumbnails")
+        self.create_videos_button.clicked.connect(self.create_all)
+        self.textbox = QPlainTextEdit()
+        self.textbox.resize(280,40)
 
-        layout.addWidget(choose_region)
-        layout.addWidget(choose_game)
-        layout.addWidget(choose_file)
-        layout.addWidget(only_thumbnails)
-        layout.addWidget(create_videos_button)
+        layout.addWidget(self.choose_region)
+        layout.addWidget(self.choose_game)
+        layout.addWidget(self.choose_stream)
+        layout.addWidget(self.choose_banner)
+        layout.addWidget(self.textbox)
+        layout.addWidget(self.only_thumbnails)
+        layout.addWidget(self.create_videos_button)
 
         widget = QWidget()
         widget.setLayout(layout)
@@ -45,6 +101,29 @@ class MainWindow(QMainWindow):
     def choose_dir(self):
         """Choose directory helper method"""
         return QFileDialog.getExistingDirectory(self, "Select Directory")
+    
+    def create_all(self):
+        user_input = self.textbox.toPlainText().split("\n")
+        for line in user_input:
+            #start_time = line.split(" ")[0]
+            #end_time = line.split(" ")[1]
+            title = line.split(" ", 2)[2]
+            try:
+                player_list, tournament_round, game_name = formatTitle(title)
+            except:
+                print("Error in line: " + line)
+                return 0
+
+
+            match = MatchInfo(game_name, tournament_round)
+            image_info = ImageInfo()
+            config = Config()
+            windows_title = title.replace("|", "¤")
+            windows_title = windows_title.replace(":", "#")
+            print(title)
+            new_thumbnail = Thumbnail(player_list, match, image_info, config, windows_title)
+            new_thumbnail.create_thumbnail()
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
--- vodgen/thumbnail.py
+++ vodgen/thumbnail.py
@@ -1,21 +1,162 @@
 """Module to create a thumbnail, given enough information"""
 
 import json
-#from PIL import ImageColor
+from PIL import ImageColor
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+from PIL import ImageOps
+from PIL import ImageChops
+import numpy as np
 
 class Thumbnail:
     """The thumbnail class, containing all necessary information to create a thumbnail,
      and methods to actually create a png"""
      # pylint: disable=too-few-public-methods
-    def __init__(self, players, match, image_info, config):
+    def __init__(self, players, match, image_info, config, title):
         self.players = players
         self.match = match
         self.image_info = image_info
         self.config = config
+        self.title = title
 
     def print_all(self, players):
         """Create all the thumbnails for comparison between left-right"""
         self.players = players
+    def create_thumbnail(self):
+        base_image = Image.open(self.config.base_dir).convert("RGBA")
+        banner = Image.open(self.config.banner_dir).convert("RGBA")
+        if len(self.players) == 4:
+            font_size = 35
+        else:
+            font_size = 60
+        font = ImageFont.truetype(self.config.font_dir, 60)
+        vs_font = ImageFont.truetype(self.config.vs_font_dir, 120)
+        player_font = ImageFont.truetype(self.config.font_dir, font_size)
+        game_name = self.match.game_name
+        draw = ImageDraw.Draw(base_image)
+        boxes = [self.image_info.player1_box, self.image_info.player2_box, self.image_info.round_box, self.image_info.game_box]
+        #for box in boxes:
+        #    draw.rectangle(box, fill=self.config.bar_color, outline=self.config.bar_color)
+        
+        with open('./characterinfo.json', encoding="utf-8") as file:
+            character_info = json.load(file)
+            print(self.players)
+            player_names = []
+            for player in self.players:
+                img = Image.open(player.characterDir).convert("RGBA")
+                offset = 0
+                
+                if len(self.players) == 4:
+                    if not "head" in character_info[game_name][player.character]:
+                        if character_info[game_name][player.character]["placement"] == "forward":
+                            width, height = img.size
+                            newWidth = round(width / 2)
+                            img.crop((0,0, newWidth, height))
+                        elif character_info[game_name][player.character]["placement"] == "backward":
+                            width, height = img.size
+                            img.crop((width*2,0, width, height))
+                        else:
+                            width, height = img.size
+                            img.crop((int(width * 0.25),0, int(width * 0.75), height))
+                    else:
+                        if character_info[game_name][player.character]["head"] == "right":
+                            width, height = img.size
+                            img = img.crop((int(width * 0.3), 0, int(width * 0.80), height))
+                        elif character_info[game_name][player.character]["head"] == "left":
+                            width, height = img.size
+                            img = img.crop((0, 0, int(width * 0.75), height))
+                        else:
+                            print("fuck")
+                
+                img = resizeCrop(img, player.character, game_name, len(self.players))
+                if player.character in character_info[game_name]:
+                    if character_info[game_name][player.character]["looking"] == "left" and player.number <= len(self.players)/2:
+                        img = ImageOps.mirror(img)
+                        print("mirroring left")
+                    elif character_info[game_name][player.character]["looking"] == "right" and int(player.number) > len(self.players)/2:
+                        img = ImageOps.mirror(img)
+                        print("mirroring right side ")
+                    if character_info[game_name][player.character]["placement"] == "forward":
+                        if player.number % 2 == 0:
+                            offset = round(320 * 0.15)
+                        else:
+                            offset = -round(320 * 0.15)
+                    elif character_info[game_name][player.character]["placement"] == "backward":
+                        if player.number % 2 == 0:
+                            offset = -round(320 * 0.15)
+                        else:
+                            offset = round(320 * 0.15)
+                
+
+                print(offset)                        
+                if len(self.players) == 4:
+                    player_map = {
+                        1: (0 - offset, 128, int(640/2) - offset, 464 + 128),
+                        2: (int(640/2) - offset, 128, 640 - offset, 464 + 128),
+                        3: (640, 128, (640+int(640/2)), 464 + 128),
+                        4: ((640+int(640/2)), 128, 1280, 464 + 128)
+                    }
+                else:
+                    player_map = {
+                        1: (0 - offset, 128, 640 - offset, 464 + 128),
+                        2: (640, 128, 1280, 464 + 128)
+                    }
+                if len(self.players) == 4:
+                    box_map = {
+                        1: (0, 0, int(640/2), 128),
+                        2: (int(640/2), 0, 640, 128),
+                        3: (640, 0, int(640*1.5), 128),
+                        4: (int(640*1.5), 0, 1280, 128)
+                    }
+                else:
+                    box_map = {
+                        1: (0, 0, 640, 128),
+                        2: (640, 0, 1280, 128)
+                    }
+                base_image.paste(img, player_map[player.number], img)
+                player_text = player.name.upper()
+                text_width, text_height = draw.textsize(player_text, font=player_font)
+                text_coordinates = center_text(box_map[player.number], text_width, text_height)
+                player_names.append({
+                    "name": player_text,
+                    "coords": text_coordinates
+                })
+
+
+            
+            base_image.paste(banner, (0,0), banner)
+            for entry in player_names:
+                draw.text(entry["coords"], entry["name"], self.config.font_color, font=player_font)
+            
+            text = "VS"
+            shadow_color = "black"
+            text_width, text_height = draw.textsize(text, font=vs_font)
+            text_coords = center_text(self.image_info.vs_box, text_width, text_height)
+            draw.text(((text_coords[0]-1, text_coords[1]-1)), text, font=vs_font, fill=shadow_color)
+            draw.text(((text_coords[0]+1, text_coords[1]-1)), text, font=vs_font, fill=shadow_color)
+            draw.text(((text_coords[0]-1, text_coords[1]+1)), text, font=vs_font, fill=shadow_color)
+            draw.text(((text_coords[0]+1, text_coords[1]+1)), text, font=vs_font, fill=shadow_color)
+            draw.text(text_coords, text, self.config.font_color, font=vs_font)
+            
+            text = self.match.tournament_round.upper()
+            text_width, text_height = font.getsize(text)
+            text_coords = center_text(self.image_info.round_box, text_width, text_height)
+            draw.text(text_coords, text, self.config.font_color, font=font)
+            
+            text = self.match.game_name_thumbnail.upper()
+            text_width, text_height = font.getsize(text)
+            text_coords = center_text(self.image_info.game_box, text_width, text_height)
+            draw.text(text_coords, text, self.config.font_color, font=font)
+
+            logo = Image.open(self.config.logo_dir).convert("RGBA")
+            logo = logo.resize((182, 181))
+            width, height = logo.size
+            coords = (547, 380, width+547, height+380)
+            base_image.paste(logo, coords, logo)
+            
+            base_image.save("./results/" + self.title + ".png")
+            
 
 
 class ImageInfo:
@@ -24,19 +165,25 @@ class ImageInfo:
      # pylint: disable=too-few-public-methods
      # pylint: disable=too-many-instance-attributes
     def __init__(self):
-        header = 128
+        self.header = 128
         self.resolution = (1280, 720)
-        self.player1_box = (0, 0, 640, header)
+        self.player1_box = (0, 0, 640, self.header)
         self.player_font_size = 60
         self.vs_font_size = 120
-        self.player2_box = (640, 0, 1280, header)
+        self.player2_box = (640, 0, 1280, self.header)
         self.round_box = (0, 592, 640, 720)
         self.game_box = (640, 592, 1280, 720)
         self.vs_box = (580, 239, 580+120, 239+110)
 class MatchInfo:
     """Contains info about the match which the thumbnail is for"""
-    def __init__(self, game_name):
-        self.tournament_round = None
+    def __init__(self, game_name, tournament_string):
+        string_array = tournament_string.split(" ")
+        tournament_round = string_array[0] + " " + string_array[1].replace("R", "Round ")
+        
+        self.tournament_round = tournament_round
+        with open('./config.json', encoding="utf-8") as file:
+            config = json.load(file)
+            self.game_name_thumbnail = config["game"][game_name + " Doubles"]["title"]
         self.game_name = game_name
 
     def set_tournament_round(self, tournament_string):
@@ -56,28 +203,39 @@ class Player:
     Contains player information
     """
     # pylint: disable=too-few-public-methods
-    def __init__(self, player_name, character):
-        self.player_name = player_name
+    def __init__(self, player_name, character, team, number):
+        self.name = player_name
         self.character = character
+        self.characterDir = None
+        self.team = team
+        self.number = number
+        fileName = self.character.lower().replace(" ", "_") + "_00.png"
+        self.characterDir = f"./assets/characters/ssbm/{self.character}/{fileName}"
+
+    def setCharacterDir(self, imageDir):
+        fileName = self.character.lower().replace(" ", "_") + "_00.png"
+        self.characterDir = f"{imageDir}/{self.character}/{fileName}"
+
 
 
 class Config:
     """Contains information about directories, and reads the config.json to get this info"""
     # pylint: disable=too-few-public-methods
     def __init__(self):
-        self.logo_dir = None
-        self.base_dir = None
-        self.font_dir = None
-        self.vs_font_dir = None
-        self.character_image_dir = None
-        self.bar_color = None
-        with open('../assets/characterinfo.json', encoding="utf-8") as file:
-            self.character_info = json.load(file)
+        self.logo_dir = "./assets/logos/smashrogaland_logo.png"
+        self.base_dir = "./assets/background/ssbm_bg.png"
+        self.font_dir = "./assets/fonts/Raleway-ExtraBold.ttf"
+        self.vs_font_dir = "./assets/fonts/agency-fb.ttf"
+        self.font_color = (255,255,255)
+        self.banner_dir = "./assets/banners/rogasmash200banner.png"
+        self.character_image_dir = "./assets/characters/ssbm"
+        self.bar_color = "#4477BD"
     def read_config(self):
         """Reads through the config.json and sets the appropriate values"""
-        with open('../assets/characterinfo.json', encoding="utf-8") as file:
+        with open('./characterinfo.json', encoding="utf-8") as file:
             self.character_info = json.load(file)
         #ImageColor(bar_color, "RGB")
+
 
 def center_text(box, text_width, text_height):
     """Centers text, given dimensions and width and height"""
@@ -87,3 +245,54 @@ def center_text(box, text_width, text_height):
     box_height = y_2 - y_1
     text_coords = ((box_width-text_width)/2+x_1,(box_height-text_height)/2+y_1-y_offset)
     return text_coords
+
+
+
+def resizeCrop(img, character, game, player_num):
+    portraitHeight = 464
+    portraitWidth = 1280 / player_num
+    #Resize image based on max height in template
+    aspectRatio = img.size[0] / img.size[1]
+    portraitResolution = (round(portraitHeight * 1.3 * aspectRatio), round(1.3 * portraitHeight))
+    characterInfo = json.load(open("characterinfo.json"))
+    
+    img = img.resize(portraitResolution)
+
+    width, height = img.size
+    offsetWidth = (width - portraitWidth)
+    if character in characterInfo[game]:
+        if characterInfo[game][character]["placement"] == "center":
+            offsetWidth /= 2
+            coords = (offsetWidth, 0, width - offsetWidth, portraitHeight)
+        elif characterInfo[game][character]["placement"] == "forward":
+            coords = (offsetWidth, 0, width, portraitHeight)
+        elif characterInfo[game][character]["placement"] == "backward":
+            coords = (0, 0, width - offsetWidth, portraitHeight)
+        elif characterInfo[game][character]["placement"] == "upward":
+            offsetHeight = round(portraitHeight * 0.2)
+            offsetWidth /= 2
+            coords = (offsetWidth, offsetHeight, width - offsetWidth, portraitHeight + offsetHeight)
+        elif characterInfo[game][character]["placement"] == "downward":
+            offsetHeight = round(portraitHeight * 0.4)
+            print("test")
+            offsetWidth /= 2
+            coords = (offsetWidth, 0, width - offsetWidth, portraitHeight)
+        else: 
+            offsetWidth /= 2
+            coords = (offsetWidth, 0, width - offsetWidth, portraitHeight)
+        #If nothing is specified in file, just center the image
+    else:
+        offsetWidth /= 2
+        coords = (offsetWidth, 0, width - offsetWidth, portraitHeight)
+
+    img = img.crop(coords)
+    return img
+
+
+if __name__ == '__main__':
+    players = [Player("ove", "Sheik", 0, 1), Player("Kise Seryuu", "Fox", 0, 2), Player("Class Ganon", "Snake", 1, 3), Player("Juice Nasty", "K Rool", 1, 4)]
+    match = MatchInfo("SSBM", "Winners R1")
+    image_info = ImageInfo()
+    config = Config()
+    thumbnail = Thumbnail(players, match, image_info, config)
+    thumbnail.create_thumbnail()
