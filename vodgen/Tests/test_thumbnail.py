f0085d03c01f5bf0b47f43a465a046163fa17fb1
oveof <ovedy@oftedal.com>
2021-12-23 01:21:51 +0100

    test

---
+++ vodgen/Tests/test_thumbnail.py
@@ -0,0 +1,9 @@
+"""Thumbnail tests"""
+import unittest
+import thumbnail
+
+class MatchInfoTests(unittest.TestCase):
+    """Tests match info methods"""
+    def tests_set_tournament_round(self):
+        """Tests the parser who shortens title"""
+        self.assertIsNotNone("lol")
